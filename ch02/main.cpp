c62cccb8962a9a646c21bc8d4cdb46008e0766f6
tangzhiyang <2684181043@qq.com>
2026-05-12 08:58:08 +0000

    feat: put all print

--- ch02/main.cpp
+++ ch02/main.cpp
@@ -1,29 +1,60 @@
 #include <dlfcn.h>
 #include <iostream>
+#include <dirent.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    void *handle = dlopen("./function.so", RTLD_LAZY);
-    if(handle == 0)
-    {
-	cout << "dlopen error" << endl;
-	return 0;
+typedef void (*FUNC_PRINT)();
+
+int main() {
+    string plugin_dir = "../plugin";
+
+    DIR* dir = opendir(plugin_dir.c_str());
+    if (!dir) {
+        cerr << "Cannot open directory: " << plugin_dir << endl;
+        return 1;
     }
 
-    typedef void (*FUNC_PRINT)();
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != nullptr) {
+        string filename = entry->d_name;
 
-    FUNC_PRINT dl_print = (FUNC_PRINT)dlsym(handle, "Print");
-    if(dl_print == 0)
-    {
-	cout << "dlsym error" << endl;
-	return 0;
+        if (filename == "." || filename == "..") continue;
+
+        if (filename.size() >= 3 && filename.substr(filename.size() - 3) == ".so") {
+            string path = plugin_dir + "/" + filename;
+
+            // 检查文件可读
+            int fd = open(path.c_str(), O_RDONLY);
+            if (fd == -1) {
+                cerr << "Cannot open file: " << path << endl;
+                continue;
+            }
+            close(fd);
+
+            // cout << "Loading: " << path << endl;
+
+            void* handle = dlopen(path.c_str(), RTLD_LAZY);
+            if (!handle) {
+                cerr << "dlopen error: " << dlerror() << endl;
+                continue;
+            }
+
+            FUNC_PRINT dl_print = (FUNC_PRINT)dlsym(handle, "Print");
+            if (!dl_print) {
+                cerr << "dlsym error: " << dlerror() << endl;
+                dlclose(handle);
+                continue;
+            }
+
+            dl_print();
+            dlclose(handle);
+        }
     }
 
-    (dl_print)();
-
-    dlclose(handle);
-
+    closedir(dir);
     return 0;
 }
